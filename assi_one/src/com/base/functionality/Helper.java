cbf946f2f6ebbed1821f6c284f35c4e515053150
Bibek Acharya <sacharya186@gmail.com>
2017-03-26 18:20:32 +0545

    made some changes

--- assi_one/src/com/base/functionality/Helper.java
+++ assi_one/src/com/base/functionality/Helper.java
@@ -47,7 +47,8 @@ public class Helper {
             } else if (option == 5) {
                 System.exit(0);
             } else {
-
+                System.out.println("Value must be between 1 and 5. Please try again:");
+                option=input.nextInt();
             }
             displayMenu();
         }
@@ -64,54 +65,70 @@ public class Helper {
 
     public void optionOneForOne(int option){
         if (option==1){
-            System.out.println("Enter call length in seconds: ");
+            System.out.println("\t\t\tEnter call length in seconds: ");
             callLength=callLength+input.nextInt();
             call++;
         }else if (option==2){
             sms=sms+1;
-            System.out.println("Total number of SMS so far = "+sms);
+            System.out.println("\t\t\tTotal number of SMS so far = "+sms);
         }else if (option==3){
-            System.out.println("Enter the amount of data in MB:");
+            System.out.println("\t\t\tEnter the amount of data in MB:");
             dataUsage=dataUsage+input.nextInt();
         }else if (option==4){
             displayMenu();
+        }else{
+            System.out.println("\t\t\tValue must be between 1 and 4. Please try again:");
+            option=input.nextInt();
+            optionOneForOne(option);
         }
+        displayMenuForOptionOne();
+        option=input.nextInt();
+        optionOneForOne(option);
     }
 
     public void planA(){
-        System.out.println("Cost under Plan A");
+        System.out.println("\t\t\tCost under Plan A");
         displayLine();
-        System.out.print("Number of calls ="+ call);
-        System.out.println("$"+call*0.23);
-        System.out.print("Total call time (secs) ="+ callLength);
-        System.out.println("$"+callLength*0.02);
-        System.out.print("Number of SMS ="+sms);
-        System.out.println("$"+sms*0.12);
-        System.out.print("Data usage (MB) ="+dataUsage);
-        System.out.println("$"+dataUsage*0.03);
+        System.out.print("\t\t\tNumber of calls ="+ call);
+        System.out.println("\t\t\t\t\t\t$"+call*0.23);
+        System.out.print("\t\t\tTotal call time (secs) ="+ callLength);
+        System.out.println("\t\t\t$"+callLength*0.02);
+        System.out.print("\t\t\tNumber of SMS ="+sms);
+        System.out.println("\t\t\t\t\t\t$"+sms*0.12);
+        System.out.print("\t\t\tData usage (MB) ="+dataUsage);
+        System.out.println("\t\t\t\t\t\t$"+dataUsage*0.03);
         displayLine();
-        System.out.println("TOTAL COST "+(call*0.23+callLength*0.02+sms*0.12+dataUsage*0.03));
+        float total= (float)(call*0.23+callLength*0.02+sms*0.12+dataUsage*0.03);
+        System.out.println("\t\t\tTOTAL COST "+"\t\t\t"+total);
+        displayLine();
     }
-
     public void planB(){
-        System.out.println("Cost under Plan B");
+        System.out.println("\t\t\tCost under Plan B");
         displayLine();
-        System.out.print("Number of calls ="+ call);
-        System.out.println("$"+call*0.17);
-        System.out.print("Total call time (secs) ="+ callLength);
-        System.out.println("$"+callLength*0.03);
-        System.out.print("Number of SMS ="+sms);
-        System.out.println("$"+sms*0.15);
-        System.out.print("Data usage (MB) ="+dataUsage);
-        System.out.println("$"+dataUsage*0.02);
+        System.out.print("\t\t\tNumber of calls ="+ call);
+        System.out.println("\t\t\t\t\t\t$"+call*0.17);
+        System.out.print("\t\t\tTotal call time (secs) ="+ callLength);
+        System.out.println("\t\t\t$"+callLength*0.03);
+        System.out.print("\t\t\tNumber of SMS ="+sms);
+        System.out.println("\t\t\t\t\t\t$"+sms*0.15);
+        System.out.print("\t\t\tData usage (MB) ="+dataUsage);
+        System.out.println("\t\t\t\t\t\t$"+dataUsage*0.02);
         displayLine();
-        System.out.println("TOTAL COST "+(call*0.17+callLength*0.03+sms*0.15+dataUsage*0.02));
+        float total= (float) (call*0.17+callLength*0.03+sms*0.15+dataUsage*0.02);
+        System.out.println("\t\t\tTOTAL COST "+"\t\t\t\t\t\t"+total);
+        displayLine();
     }
     public void clearUsage(){
         sms=0;
         call=0;
         callLength=0;
         dataUsage=0;
+        displayLine();
+        System.out.println("\t\t\tALL USAGE DETAILS HAVE BEEN RESET TO 0");
+        displayLine();
+    }
+    private void displayAsDollarsAndCents(int cents){
+        System.out.print("$" + (cents/100) + "." + (cents%100));
     }
 }
 
